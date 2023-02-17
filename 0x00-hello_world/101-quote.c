b44a93e08cc1c726e515c9bdca45633b17a09ddf
Olayinkaspec <oyinkansolahod@gmail.com>
2023-02-16 18:30:49 -0800

    standard error updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-#include <unistd.h>
 /**
  * main - A program that prints a line to the standard error
  * Return: 1 (Success)
  */
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 return (1);
 }
