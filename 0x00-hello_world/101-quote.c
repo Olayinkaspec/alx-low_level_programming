92ff9af6df46d80586f1aa05fe3d41961cd768a6
Olayinkaspec <oyinkansolahod@gmail.com>
2023-02-16 18:43:15 -0800

    updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - A program that prints a line to the standard error
  * Return: 1 (Success)
  */
 int main(void)
 {
-fwrite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 return (1);
 }
