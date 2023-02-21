4990550d8f371c7786ee726b56476bd7846d516d
Olayinkaspec <oyinkansolahod@gmail.com>
2023-02-21 14:52:09 -0800

    absolutely

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _abs - computes an integer's absolute value
+ * @ab: integer input
+ * Return: absolute value of ab
+ */
+int _abs(int ab)
+{
+return (ab * ((ab > 0) - (ab < 0)));
+}
