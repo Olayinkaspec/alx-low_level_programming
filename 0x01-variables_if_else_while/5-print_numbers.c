6c057567a364600dacfbaf718db0a44171bddd68
Olayinkaspec <oyinkansolahod@gmail.com>
2023-02-21 08:55:29 -0800

    digit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers of base 10
+ * Return:0 (Success)
+ */
+int main(void)
+{
+int a;
+for (a = 0; a <= 10; a++)
+printf("%d", a);
+putchar('\n');
+return (0);
+}
