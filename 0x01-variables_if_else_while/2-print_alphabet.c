1c9b5458dfe37993698429574ea55bd23f6f677e
Olayinkaspec <oyinkansolahod@gmail.com>
2023-02-20 16:48:44 -0800

    lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet in lower case
+ * Return:0 (Success)
+ */
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++);
+putchar(c);
+putchar('\n');
+return (0);
+}
