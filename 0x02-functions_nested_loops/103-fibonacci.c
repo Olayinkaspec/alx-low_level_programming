16f5e3fc64698c1fbb09c2143eac57d56ec5e3be
Olayinkaspec <oyinkansolahod@gmail.com>
2023-02-22 15:17:47 -0800

    even Fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints the sum of the even-valued Fibonacci numbers
+ * less than 4000000
+ * Return: Nothing!
+ */
+int main(void)
+{
+int x = 0;
+long y = 1, z = 2, sum = z;
+while (z + y < 4000000)
+{
+z += y;
+if (z % 2 == 0)
+sum += z;
+y = z - y;
+++x;
+}
+printf(", %ld\n", sum);
+return (0);
+}
