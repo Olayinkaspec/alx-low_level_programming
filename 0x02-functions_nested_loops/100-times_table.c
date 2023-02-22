94138c802737115bbfb9908680f6916befb8e90e
Olayinkaspec <oyinkansolahod@gmail.com>
2023-02-22 12:15:28 -0800

    times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_times_table - prints the n times table, starting with 0
+ * @n: number input
+ * Return: 0 (success)
+ */
+void print_times_table(int n)
+{
+print_times_table(3);
+_putchar('\n');
+print_times_table(5);
+_putchar('\n');
+print_times_table(98);
+_putchar('\n');
+print_times_table(12);
+}
