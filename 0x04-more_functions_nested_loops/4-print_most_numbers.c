9b0702e61ca82c59e37b1503fd9615f4b884f7f3
Olayinkaspec <oyinkansolahod@gmail.com>
2023-02-27 01:54:04 -0800

    do not print the other numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,14 +6,10 @@
  */
 void print_most_numbers(void)
 {
-int i = 0;
-for (; i = <= 9; i++)
+int i;
+for (i = 0; i < 10; i++)
 {
-if (i == 2 || i == 4)
-{
-continue;
-}
-else
+if (i != 2 && i != 4)
 {
 _putchar(i + '0');
 }
