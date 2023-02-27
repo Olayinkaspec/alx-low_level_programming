af03eaf5e442ace66b1b432dc9e98f72045c9bce
Olayinkaspec <oyinkansolahod@gmail.com>
2023-02-27 05:58:21 -0800

    FizzBuzz Test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,26 +9,16 @@ int main(void)
 int b;
 for (b = 1; b <= 100; b++)
 {
-if (b % 3 == 0 && b % 5 != 0)
-{
+if (((b % 3) == 0) && ((b % 5) != 0))
 printf("Fizz");
-}
-else if (b % 5 == 0 && b % 3 != 0)
-{
+else if (((b % 5) == 0) && ((b % 3) != 0))
 printf("Buzz");
-}
-else if (b % 3 == 0 && b % 5 == 0)
-{
+else if (((b % 5) == 0) && ((b % 3) == 0))
 printf("FizzBuzz");
-}
-else if (b == 1)
-{
-printf("%d", b);
-}
 else
-{
 printf("%d", b);
-}
+if (!(b == 100))
+printf(" ");
 printf("\n");
 return (0);
 }
