918bfc6d8c18c806098e480c55f7f2b1e5819d80
Olayinkaspec <oyinkansolahod@gmail.com>
2023-02-27 06:05:20 -0800

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,16 +10,24 @@ int b;
 for (b = 1; b <= 100; b++)
 {
 if (((b % 3) == 0) && ((b % 5) != 0))
+{
 printf("Fizz");
 else if (((b % 5) == 0) && ((b % 3) != 0))
+{
 printf("Buzz");
+}
 else if (((b % 5) == 0) && ((b % 3) == 0))
+{
 printf("FizzBuzz");
+}
 else
+{
 printf("%d", b);
+}
 if (!(b == 100))
 printf(" ");
 printf("\n");
 return (0);
+}
 }
 }
