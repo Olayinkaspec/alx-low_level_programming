0bd4daa45fcba4930c04d9b29a81782601ddd122
Olayinkaspec <oyinkansolahod@gmail.com>
2023-02-28 09:27:22 -0800

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Takes a pointer to an int as parameter
+ * updates the value it points to to 98
+ * @n: An integer input pointer
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifndef OLAYINKA_H
-#define OLAYINKA_
+#define OLAYINKA_H
 
-int _putchar(char c)
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -10,7 +10,8 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
-void puts_half(char *str);
+void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 
 #endif
