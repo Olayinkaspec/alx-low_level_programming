911ec8bcd9961bb516e069b44e9f911a21da5dbb
Olayinkaspec <oyinkansolahod@gmail.com>
2023-02-28 15:28:28 -0800

    password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,15 +8,15 @@
 int main(void)
 {
 int pass[100];
-int b, sum, n;
+int i, sum, n;
 sum = 0;
 srand(time(NULL));
-for (b = 0; b < 100; b++)
+for (i = 0; i < 100; i++)
 {
-pass[b] = rand() % 78;
-sum += (pass[b] + '0');
-putchar(pass[b] + '0');
-if ((2722 - sum) - '0' < 78)
+pass[i] = rand() % 78;
+sum += (pass[i] + '0');
+putchar(pass[i] + '0');
+if ((2772 - sum) - '0' < 78)
 {
 n = 2772 - sum - '0';
 sum += n;
