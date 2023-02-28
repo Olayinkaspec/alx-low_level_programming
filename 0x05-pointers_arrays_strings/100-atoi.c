73323002ffb3e40dc2d2ccaf01733bc173657925
Olayinkaspec <oyinkansolahod@gmail.com>
2023-02-28 14:13:28 -0800

    return integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _atoi - A function that converts a string to an integer
+ * @s: pointer to convert
+ * Return: integer
+ */
+int _atoi(char *s)
+{
+int c = 0;
+unsigned int ni = 0;
+int min = 1;
+int isi = 0;
+while (s[c])
+{
+if (s[c] == 45)
+{
+min *= -1;
+}
+while (s[c] >= 48 && s[c] <= 57)
+{
+isi = 1;
+ni = (ni * 10) + (s[c]) - '0';
+c++;
+}
+}
+}
