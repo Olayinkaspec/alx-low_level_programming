384582d6b4ad445be5aa294bf9d6500b2016291c
Olayinkaspec <oyinkansolahod@gmail.com>
2023-03-20 16:06:54 -0700

    prints the name of the file it was compiled from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints the name of a file
+ * followed by a new line
+ * Return: 0(Success)
+ */
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
